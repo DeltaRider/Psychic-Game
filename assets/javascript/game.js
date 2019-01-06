1992cac420d8cc0c32b311ea749e7ec6be13064c
DeltaRider <deltariderllc@gmail.com>
2019-01-05 16:34:11 -0800

    JS src script  added to HMTML

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+alert (`Welcome to the Psychic Game!
+
+You have 9 guesses to guess the letter I'm thinking of.
+You can guess any letter in the alphabet.
+You cannot guess the same letter twice.
+If you guess correctly, you win!
+If don't guess within 9 tries, you lose...
+
+Good Luck.`)
+
