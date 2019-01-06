1f32e912780658a4ecbbca5ab1b5f7606d1f51ab
DeltaRider <deltariderllc@gmail.com>
2019-01-05 17:27:44 -0800

    onkey guess letter added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,6 +11,17 @@ Good Luck.`)
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-var lettersGuessed = [""]
-var letterOnMind = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var lettersGuessed = [""];
+var lettersArr = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var ranNum = Math.floor(Math.random()*lettersArr.length);
+var letterToGuess = lettersArr[ranNum];
 
+document.onkeyup = function(){
+    if (letterToGuess.includes(event.key)){
+        wins++;
+        document.querySelector('#wins').innerText = "Wins: " + wins;
+    } else {
+        guessesLeft--;
+        document.querySelector('#tries').innerText = "Guesses Left: " + guessesLeft;
+    }
+}
